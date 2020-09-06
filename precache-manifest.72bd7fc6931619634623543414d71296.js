9b6bd38eff0c6afea43db130f14f3807f5a6f89a
Dell <Dell@PROSOFT101>
2020-09-06 11:31:46 +0530

    Updates

---
+++ precache-manifest.72bd7fc6931619634623543414d71296.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68cef0a00ad5acfcb77f4854a421e897",
+    "url": "/myreactwebsite/index.html"
+  },
+  {
+    "revision": "baf68d5478997336f5a0",
+    "url": "/myreactwebsite/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "1ca2ffed2b04ab9c12c2",
+    "url": "/myreactwebsite/static/css/main.f30a1f8e.chunk.css"
+  },
+  {
+    "revision": "baf68d5478997336f5a0",
+    "url": "/myreactwebsite/static/js/2.0f802c1d.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/myreactwebsite/static/js/2.0f802c1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca2ffed2b04ab9c12c2",
+    "url": "/myreactwebsite/static/js/main.95e9a7e2.chunk.js"
+  },
+  {
+    "revision": "12ed5ad0940d849061e1",
+    "url": "/myreactwebsite/static/js/runtime-main.957fd87f.js"
+  },
+  {
+    "revision": "2bbdd021e8678d088cd5a78853744a4f",
+    "url": "/myreactwebsite/static/media/android.2bbdd021.jpg"
+  },
+  {
+    "revision": "a5f7b9f695af0c074872e10a39492de1",
+    "url": "/myreactwebsite/static/media/app.a5f7b9f6.jpg"
+  },
+  {
+    "revision": "9a15be211ed2df3f7191358f1db63472",
+    "url": "/myreactwebsite/static/media/digital.9a15be21.jpg"
+  },
+  {
+    "revision": "38e24bd602a42abe72e8abf72e430592",
+    "url": "/myreactwebsite/static/media/img3.38e24bd6.jpg"
+  },
+  {
+    "revision": "1f0bc6523068b45a4ac51a893257086f",
+    "url": "/myreactwebsite/static/media/img4.1f0bc652.jpg"
+  },
+  {
+    "revision": "e8fb0ddd7d7bc2f447b8a9a071d13a87",
+    "url": "/myreactwebsite/static/media/marketing.e8fb0ddd.jpg"
+  },
+  {
+    "revision": "4b837e5e1fdffe0208b557eddcc06223",
+    "url": "/myreactwebsite/static/media/meeting.4b837e5e.jpg"
+  },
+  {
+    "revision": "1f0bc6523068b45a4ac51a893257086f",
+    "url": "/myreactwebsite/static/media/software.1f0bc652.jpg"
+  },
+  {
+    "revision": "fc664ed6812d4a4158e34e5040ad3654",
+    "url": "/myreactwebsite/static/media/web.fc664ed6.jpg"
+  }
+]);
